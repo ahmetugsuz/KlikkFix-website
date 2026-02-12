c55da9be9bc24300289eaad3bbb081d24e1e51fb
Ahmet Tugsuz <ahmettugsuz@Mac.home>
2026-02-12 21:29:32 +0100

    Added compatibility for mobile screen

--- components/MobileGate.tsx
+++ components/MobileGate.tsx
@@ -8,7 +8,7 @@ export default function MobileGate() {
 
       <div className="relative z-10 flex flex-col items-center">
         <Image
-          src="/favicon.ico" 
+          src="/favicon/apple-touch-icon.png" 
           alt="KlikkFix Logo"
           width={80}
           height={80}
